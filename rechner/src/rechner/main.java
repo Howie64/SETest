cb21a6227ea2eb094729af7b7a28cbb2641c76c3
Martin Steffen <m.steffen1337@yahoo.de>
2014-12-10 11:16:37 +0100

    Exception Handling added.

--- rechner/src/rechner/main.java
+++ rechner/src/rechner/main.java
@@ -6,17 +6,27 @@ public class main {
 
 	public static void main(String[] args)
 	{
+		Integer input1 = 0;
+		Integer input2 = 0;
 		Scanner in = new Scanner(System.in);
 		
 		System.out.print("Enter Int 1:");
 		
-		Integer input1 = in.nextInt();
+		try
+		{
+			input1 = in.nextInt();
+			System.out.print("Enter Int 2:");
+			input2 = in.nextInt();
+			System.out.print("Sum: " + (input1 + input2));
+		}
+		catch(Exception e)
+		{
+			System.out.print("Bitte Zahlen eingeben !");
+		}
 		
-		System.out.print("Enter Int 2:");
 		
-		Integer input2 = in.nextInt();
 
-		System.out.print("Sum: " + (input1 + input2));
+		
 	}
 
 }
